6fd2c2672f188740648f77621b1b300549ea2f50
Liz Kerber <emkerber@outlook.com>
2016-07-08 00:10:17 -0500

    Added a jQuery animation

--- public/scripts.js
+++ public/scripts.js
@@ -1,20 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $('#balanceButton').on('click', function(){
+    $('#balanceButton').on('click', function() {
 
-    $.ajax({
-      url: '/balance',
-      method: 'GET'
-    }).then(function(response){
-      $("#appendBalance").empty();
-      $('#appendBalance').append(response);
-      console.log(response);
-    }).fail(function(response){
-      console.log('error', response);
-    })
+        $.ajax({
+            url: '/balance',
+            method: 'GET'
+        }).then(function(response) {
+            $('#appendBalance').empty();
+            $('#appendBalance').append(response).hide().fadeIn();
+            console.log(response);
+        }).fail(function(response) {
+            console.log('error', response);
+        });
 
-  })
-
-
+    });
 
 });
